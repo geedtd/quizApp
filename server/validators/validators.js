c02a0a43003d96e2971fa51f9585175c33c409a6
geedtd <grcazares@gmail.com>
2022-10-27 17:34:08 -0700

    created model for User

---
+++ server/validators/validators.js
@@ -0,0 +1,34 @@
+const isEmpty = require('is-empty')
+const validator = require('validator')
+
+module.exports.loginValidator = (data) => {
+    const errors = {}
+    let emailError = validator.isEmpty(data.email) ? 'Email is required' : (!validator.isEmail(data.email) ? 'Please provide a valid email' : '')
+    let passwordError = validator.isEmpty(data.password) ? 'Password is required' : ''
+
+    if (emailError) errors.email = emailError
+    if (passwordError) errors.password = passwordError
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+}
+
+module.exports.registerValidator = (data) => {
+    const errors = {}
+
+    let emailError = validator.isEmpty(data.email) ? 'Email is required' : (!validator.isEmail(data.email) ? 'Please provide a valid email' : '')
+    let passwordError = validator.isEmpty(data.password) ? 'Password is required' : ''
+    let firstNameError = validator.isEmpty(data.firstName) ? 'Name is required' : ''
+    let lastNameError = validator.isEmpty(data.lastName) ? 'Last name is required' : ''
+
+    if (emailError) errors.email = emailError
+    if (passwordError) errors.password = passwordError
+    if (firstNameError || lastNameError) errors.firstName = 'Full name is required'
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+}
