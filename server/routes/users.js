9a8ec61a5f12a28414113e3b50874f88ec9e0329
geedtd <grcazares@gmail.com>
2022-10-27 23:33:51 -0700

    starting on frontend

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,36 +1,36 @@
-const express = require('express')
-const Users = require('../models/User')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const checkAuth = require('../middleware/check-auth')
-const {loginValidator, regiterValidator, registerValidator } = require('../validators/validators')
+const express = require('express');
+const Users = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const checkAuth = require('../middleware/check-auth');
+const { loginValidator, registerValidator } = require("../validators/validators");
 
-const router = express.Router()
+const router = express.Router();
 
 router.post('/login', (req, res) => {
-    const {errors, isValid} = loginValidator(req.body)
+    const { errors, isValid } = loginValidator(req.body);
     if (!isValid) {
-        res.json({ success: false, errors})
+        res.json({ success: false, errors });
     } else {
         Users.findOne({ email: req.body.email }).then(user => {
             if (!user) {
-                res.json({message: "Email does not exist", success: false})
+                res.json({ message: 'Email does not exist', success: false });
             } else {
                 bcrypt.compare(req.body.password, user.password).then(success => {
                     if (!success) {
-                        res.json({ message: "Invalid password", success: false })
+                        res.json({ message: 'Invalid password', success: false });
                     } else {
-                        const payload = { 
+                        const payload = {
                             id: user._id,
                             name: user.firstName
                         }
                         jwt.sign(
                             payload,
-                            process.env.APP_SECRET, {expiresIn: 2155926},
+                            process.env.APP_SECRET, { expiresIn: 2155926 },
                             (err, token) => {
                                 res.json({
                                     user,
-                                    token: "Bearer token : " + token,
+                                    token: 'Bearer token: ' + token,
                                     success: true
                                 })
                             }
@@ -43,38 +43,38 @@ router.post('/login', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-    const {errors, isValid } = registerValidator(req.body)
-    if (!isValid ) {
-        res.json( {success: false, errors})
-    } else { 
-        const {firstName, lastName, email, password } = req.body 
-        const registeredUser = new Users({
-            firstName, 
-            lastName, 
-            email, 
-            password, 
+    const { errors, isValid } = registerValidator(req.body);
+    if (!isValid) {
+        res.json({ success: false, errors });
+    } else {
+        const { firstName, lastName, email, password } = req.body;
+        const registerUser = new Users({
+            firstName,
+            lastName,
+            email,
+            password,
             createdAt: new Date()
-        })
+        });
         bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(registeredUser.password, salt, (hashErr, hash) => {
-                if (err || hashErr ) {
-                    res.json({message: "Error occured hashing", success: false})
-                return
+            bcrypt.hash(registerUser.password, salt, (hashErr, hash) => {
+                if (err || hashErr) {
+                    res.json({ message: 'Error occured hasing', success: false });
+                    return;
                 }
-                registeredUser.password = hash
-                registeredUser.save().then(() => {
-                    res.json({"message":"User created sucessfully", "success": true})
-                }).catch(err => res.json({message: err.message, success: false}))
+                registerUser.password = hash;
+                registerUser.save().then(() => {
+                    res.json({ "message": "User created successfully", "success": true });
+                }).catch(er => res.json({ message: er.message, success: false }));
             })
         })
     }
 })
 
 router.get('/:id', checkAuth, (req, res) => {
-    Users.findOne({_id: req.params.id }).then(user => {
+    Users.findOne({ _id: req.params.id }).then(user => {
         res.json({ user, success: true })
-    }).catch( err => {
-        res.json({ success: false, message: err.message})
+    }).catch(er => {
+        res.json({ success: false, message: er.message });
     })
 })
 
