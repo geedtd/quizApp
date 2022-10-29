a3796e447f12c065151b80d05cddbf0bd3b9a45a
geedtd <grcazares@gmail.com>
2022-10-28 22:24:51 -0700

    updated components to routerv6

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 
 
 class App extends React.Component {
   render() {
     return ( 
       <div class="app">
-        
+      <Router>
+        <Routes>
+          <Route exact path='/'/>
+          <Route 
+            path='*'
+            element={<Navigate to='/' replace/>}
+          />
+        </Routes>
+      </Router>
+
       </div>
     )
   }
