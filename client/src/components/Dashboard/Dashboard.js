3b7344b26f8c793b04f7ba790b1d660e2249d5fc
geedtd <grcazares@gmail.com>
2022-11-03 02:38:07 -0700

    added some styling to navbar

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -9,6 +9,17 @@ export default class Dashboard extends React.Component {
             <div className="navbar">
                 <Navbar/>
             </div>
+            <div className="main">
+                <div className="top">
+                    <div className="left">
+                        <div className="header">LEFT Stats</div>
+                    </div>
+                    <div className="right">
+                        <div className="header">RIGHT My Quizzes</div>
+                    </div>
+                </div>
+                <div className="bottom">BOTTOM</div>
+            </div>
             </div>
         )
     }
