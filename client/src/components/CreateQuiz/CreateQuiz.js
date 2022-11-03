41e68b015ec7b9129f3e1607bf77c93689610a71
geedtd <grcazares@gmail.com>
2022-11-03 04:24:39 -0700

    working on CreateQuiz functionality

--- client/src/components/CreateQuiz/CreateQuiz.js
+++ client/src/components/CreateQuiz/CreateQuiz.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import './CreateQuiz.css'
+import Navbar from '../Navbar/Navbar'
+import Dialog from '../Dialog/Dialog'
+import axios from 'axios'
 
 export default class CreateQuiz extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-
+            categories: ['Math', 'Science', 'History'],
+            categoryVal: 'Math',
+            mustBeSignedIn: false,
+            questions: [],
+            name: '',
+            addQuestions: false,
+            answers: [],
+            correctAnswer: ''
         }
     }
 
