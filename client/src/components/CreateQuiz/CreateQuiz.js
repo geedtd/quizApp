b09d2c9816a1f82488c16237adafdac1fb86571d
geedtd <grcazares@gmail.com>
2022-11-03 05:10:03 -0700

    quiz name, category chooser, and logged in to take checkbox created, and taking props

--- client/src/components/CreateQuiz/CreateQuiz.js
+++ client/src/components/CreateQuiz/CreateQuiz.js
@@ -19,6 +19,16 @@ export default class CreateQuiz extends React.Component {
         }
     }
 
+    selectPrivate = e => {
+        if(e.target.checked === true) {
+            this.setState({
+                mustBeSignedIn: e.target.checked
+            })
+        } else {
+            this.setState({mustBeSignedIn: false})
+        }
+    }
+
     render() {
         return (
             <div className="createquiz-wrapper">
@@ -28,7 +38,28 @@ export default class CreateQuiz extends React.Component {
                 <div className="main">
                     <div className="header">Create a Quiz</div>
                     <div className="form card">
-                        <input type="text" />
+                        <input type='text' className='input' onChange={e => this.setState({name: e.target.value})} value={this.state.name} placeholder='Enter Quiz Name'/>
+                        <br></br>
+                        <select 
+                            className='input select' 
+                            value={this.state.categoryVal} 
+                            onChange={ e => this.setState({categoryVal: e.target.value})}
+                            placeholder=''
+                        >
+                            <option disabled value=''>Select Category</option>
+                            {this.state.categories.map((cat, idx) => (
+                                <option key={idx} value={cat}>{cat}</option>
+                            ))}
+                        </select>
+                        <div className="checkbox">
+                            <span>User Must Be Logged In To Take</span>
+                            <input 
+                                type="checkbox"
+                                placeholder='Must Be Logged In To Take'
+                                checked={this.state.mustBeSignedIn}
+                                onChange={this.selectPrivate}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
